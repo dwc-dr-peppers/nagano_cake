b70dc2b272f916092f4172e2d4a7a229d1363a3c
crack-von-wiegenlied <110370914+crack-von-wiegenlied@users.noreply.github.com>
2022-09-19 09:26:06 +0000

    [add]商品登録機能（レイアウトは未完成）

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,39 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+    #リダイレクト先は後で修正します
+  end
+
+  def update
+
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :is_sale, :name, :introduction, :price, :image)
+  end
+
+
+
+
 end
