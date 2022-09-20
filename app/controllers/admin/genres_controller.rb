a28d609abdd4f8a79ca3d57a1b0c48f0933c1858
kobassann <108574108+kobassann@users.noreply.github.com>
2022-09-20 04:06:52 +0000

    ジャンル一覧、編集機能追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -4,20 +4,26 @@ class Admin::GenresController < ApplicationController
     @genre.save
      redirect_to admin_genres_path(@genre.id)
   end
-  
+
   def index
     @genre = Genre.new
     @genres = Genre.all
   end
-  
+
   def edit
     @genre = Genre.find(params[:id])
   end
-  
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+      redirect_to admin_genres_path(@genre)
+  end
+
   private
-  
+
   def genre_params
     params.require(:genre).permit(:name)
   end
-  
+
 end
