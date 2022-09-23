9d93cc6e5f0a920922a497665026d9610eefe0fb
EC2 Default User <ec2-user@ip-172-31-24-28.ap-northeast-1.compute.internal>
2022-09-23 15:38:18 +0000

    [Add]orders_controllerのconfirm,createアクションを定義中だが中断のためcommit

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,16 +6,28 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    #@total_amount = "0"
-    @cart_items = current_customer.cart_items.all
     @order = Order.new(order_params)
-    @order.postage = 800
+    @total = 0
+    @postage = 800
+    @totalamount = 0
+    @cart_items = current_customer.cart_items.all
+    @totalamount = @order.total_amount
+  end
+
+  def complete
+  end
+
+  def create
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.order.new(order_params)
+    @postage = 800
+    @order.postage = @postage
+    @order.payment_way = params[:order][:payment_method]
     if params[:order][:address_select] == "1"
       @order.shipping_name = current_customer.family_name + current_customer.first_name
       @order.address = current_customer.address
       @order.postcode = current_customer.postcode
     elsif params[:order][:address_select] == "2"
-      @order = Order.new(order_params)
       @address = current_customer.shipping_addresses.find_by(id: params[:order][:address_id])
       @order.postcode = @address.postcode
       @order.address = @address.address
@@ -28,34 +40,20 @@ class Public::OrdersController < ApplicationController
       shipping_address.customer_id = current_customer.id
       shipping_address.save
     end
-    @total_amount = @order.total_amount
-  end
-
-  def complete
-  end
-
-  def create
-    cart_items = current_customer.cart_items.all
-    @order = current_customer.order.new(order_params)
-    if @order.save
-      cart_items.each do |cart|
-        order_details = OrderDetails.new
-        order_details.item_id = cart_items.item_id
-        order_details.order_id = @order.id
-        order_details.quantity = cart.quantity
-        order_details.purchase_price = cart_items.item.price
-        order_details.save
-      end
-      redirect_to orders_conplete_path
-      cart_items.destroy_all
-    else
-      @order = Order.new(order_params)
-      render :new
+    @order.save
+    cart_items.each do |cart|
+      order_details = OrderDetails.new
+      order_details.item_id = cart.item_id
+      order_details.order_id = @order.id
+      order_details.quantity = cart.quantity
+      order_details.purchase_price = cart.item.price
     end
+    order_details.save
+    redirect_to orders_conplete_path
+    cart_items.destroy_all
   end
 
   def show
-    @order = Order.find(params[:id])
   end
 
   def index
