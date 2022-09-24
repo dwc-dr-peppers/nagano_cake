0671364f4025a20054f0de45990d48b318242d65
s-keito <110315888+s-keito@users.noreply.github.com>
2022-09-24 11:18:23 +0000

    [add]管理者側商品一覧

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,9 +1,10 @@
 class Admin::HomesController < ApplicationController
 
   def top
-  end
-
-  def about
+    @orders = Order.all
+		@customers = Customer.where(is_valid: true)
+		@order = Order.where.not(order_status: 0).where.not(order_status: 4)
+		@new_order = Order.last
   end
 
 end
