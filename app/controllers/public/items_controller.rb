1d045dabbf8c24ac02265aad44a39e854e588a88
s-keito <110315888+s-keito@users.noreply.github.com>
2022-09-24 00:56:29 +0000

    [add]ジャンル別カウントまだ

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,7 +6,12 @@ class Public::ItemsController < ApplicationController
   end
 
   def index
-    @item_count = Item.count
+    # if
+    #   @item_count = Genre.find(params[:name]).items.count
+    # else
+      @item_count = Item.count
+    # end
+
     @items = Item.all
     @genres = Genre.all
     @items = params[:name].present? ? Genre.find(params[:name]).items : Item.all
