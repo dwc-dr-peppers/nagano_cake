c7129302959be8706c50b7b49c67c55252b6ddc2
kobassann <108574108+kobassann@users.noreply.github.com>
2022-09-25 11:10:07 +0000

    レイアウトあと少し

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,8 @@
 class Admin::CustomersController < ApplicationController
+  
   def index
-    @customer = Customer.all
-    
+    #@customer = Customer.all
+    @customer = Customer.page(params[:page])
   end
   
   def show
@@ -14,6 +15,15 @@ class Admin::CustomersController < ApplicationController
   
   def update
     @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+       redirect_to admin_customer_path(@customer)
+    else
+       render :edit
+    end
   end
   
+  private
+  def customer_params
+  params.require(:customer).permit(:family_name, :first_name_kana, :family_name_kana, :first_name_kana, :postcode, :address, :telephone_number, :email)
+  end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,7 @@
 class Genre < ApplicationRecord
 
   has_many :items
+  
+  validates :name, presence: true, length: {maximum: 20, minimum: 2}
 
 end
