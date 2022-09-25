de9d8b672ae35fb97deb4b3605e181797ad143af
crack-von-wiegenlied <110370914+crack-von-wiegenlied@users.noreply.github.com>
2022-09-25 08:36:10 +0000

    [add]ジャンル検索にページネーション追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,19 +3,27 @@ class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
 
   def index
+    @genres = Genre.all
+    @items = params[:name].present? ? Genre.find(params[:name]).items : Item.page(params[:page]).per(6)
+
     if params[:name]
       @item_count = Genre.find(params[:name]).items.count
+      @items = Genre.find(params[:name]).items.page(params[:page]).per(6)
+      @index_name = Genre.find(params[:name]).name
+
     else
       @item_count = Item.count
+      @items = Item.page(params[:page]).per(6)
+      @index_name = "商品"
+
     end
 
-    @items = Item.all
-    @genres = Genre.all
-    @items = params[:name].present? ? Genre.find(params[:name]).items : Item.all
   end
+
 
   private
 
