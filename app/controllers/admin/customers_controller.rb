4fbcb7a126cb46ed22a1ec4e339890a43c45f816
crack-von-wiegenlied <110370914+crack-von-wiegenlied@users.noreply.github.com>
2022-09-26 10:10:34 +0000

    [add]before_action追記

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,18 +1,19 @@
 class Admin::CustomersController < ApplicationController
-  
+  before_action :authenticate_admin!
+
   def index
     #@customer = Customer.all
     @customer = Customer.page(params[:page])
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
@@ -21,7 +22,7 @@ class Admin::CustomersController < ApplicationController
        render :edit
     end
   end
-  
+
   private
   def customer_params
   params.require(:customer).permit(:family_name, :first_name_kana, :family_name_kana, :first_name_kana, :postcode, :address, :telephone_number, :email)
